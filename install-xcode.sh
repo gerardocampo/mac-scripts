c6ce014b4fc7f87fc7b11b1c181a60f5c56ed3d6
gerardocampo <gerardocampo@users.noreply.github.com>
2023-02-01 10:52:57 -0500

    Update install-xcode.sh

--- install-xcode.sh
+++ install-xcode.sh
@@ -3,6 +3,14 @@
 
 swupdate="/usr/sbin/softwareupdate"
 
+getc() {
+  local save_state
+  save_state="$(/bin/stty -g)"
+  /bin/stty raw -echo
+  IFS='' read -r -n 1 -d '' "$@"
+  /bin/stty "${save_state}"
+}
+
 ring_bell() {
   # Use the shell's audible bell.
   if [[ -t 1 ]]
